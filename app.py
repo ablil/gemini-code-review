3bbf13519979028f87ab831b0f5f6ab79a615abd
ablil <ablil@proton.me>
2024-12-01 15:23:58 +0100

    first implementation

---
+++ app.py
@@ -0,0 +1,23 @@
+#!usr/bin/env python3
+
+import ai
+import gh
+import os
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+if __name__ == '__main__':
+    assert 'GITHUB_TOKEN' in os.environ
+    assert 'GEMINI_API_KEY' in os.environ
+    assert 'GITHUB_REPOSITORY' in os.environ
+    assert 'GITHUB_REF_NAME' in os.environ and os.environ['GITHUB_REF_NAME'].endswith('/merge')
+
+    repo = os.environ['GITHUB_REPOSITORY']
+    pr_no = int(os.environ['GITHUB_REF_NAME'].split('/')[0])
+
+    pr = gh.get_pull_request(repo, pr_no)
+    diffs = gh.extract_git_diff_from_pull_request(pr)
+    for diff in diffs:
+        review = ai.ask(diff.diff)
+        gh.write_comment(pr, review, diff.filename)
