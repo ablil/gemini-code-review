14e86caa4c7d8776b5b1665805fa3a89a62230d6
ablil <ablil@proton.me>
2024-12-02 17:39:54 +0100

    refactor

--- ai.py
+++ ai.py
@@ -11,12 +11,12 @@ logging.basicConfig(level=logging.DEBUG)
 __prompt = """
 Review only the changes in the following git diff and provide clear, concise suggestions for improvement. Focus on aspects like readability, performance, and maintainability in the changed code. Include code samples or refactor suggestions where applicable. Do not review the unchanged code, and avoid unnecessary explanations—just actionable feedback on the modifications. Here’s the diff:"""
 
-@cache
-def configure_credentials(api_key: str):
+def configure_credentials(api_key: str = os.environ['GEMINI_API_KEY']):
+    logging.info("configuring credentials")
     genai.configure(api_key=api_key)
 
 def ask(git_diff: str) -> str:
-    configure_credentials(os.environ['GEMINI_API_KEY'])
+    configure_credentials()
     logging.debug("Asking Gemini AI")
     model = genai.GenerativeModel("gemini-1.5-flash")
     response = model.generate_content(f"{__prompt}\n\n{git_diff}")
